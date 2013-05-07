b65b5393b9306593744d8c984da3f7f090b816c1
Vinit Kumar <vinitcool76@gmail.com>
2013-05-07 14:53:06 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+version = '1.3'
+
+setup(
+    name='cmsplugin-fbalbumlight',
+    version=version,
+    description='facebook album plugin for django-cms with lightbox',
+    author='Vinit Kumar',
+    author_email='vinit.kumar@changer.nl',
+    url='http://github.com/vinitcool76/cmsplugin-fbalbum.git',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.2',
+        'django-cms',
+    ],
+)
